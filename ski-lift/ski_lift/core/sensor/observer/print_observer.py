9aeb3a0312b859fd2d294c2389b7c5d83e29c7e6
andras.janko <andras.janko@quadron.hu>
2024-11-10 21:39:45 +0100

    cleanup and example lift config envs

--- ski-lift/ski_lift/core/sensor/observer/print_observer.py
+++ ski-lift/ski_lift/core/sensor/observer/print_observer.py
@@ -4,4 +4,4 @@ from ski_lift.core.sensor.factory.base_sensor import SensorReading
 class PrintObserver(SensorObserver):
     """Simply prints sensor readings to console."""
     def update(self, reading: SensorReading) -> None:
-        print(f"New reading from {reading.location}: {reading.value}°C at {reading.timestamp}")
+        print(f"New {reading.sensor_type} from {reading.location}: {reading.value} at {reading.timestamp}")
