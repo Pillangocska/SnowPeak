eda23dd8412a21bb14bddefeb10b124cd3d83df0
Tarcza Lídia <100163235+diatrcz@users.noreply.github.com>
2024-11-13 23:46:27 +0100

    rabbitmq connection to backend

--- backend/src/main/java/hu/snowpeak/server/SnowPeakServerApplication.java
+++ backend/src/main/java/hu/snowpeak/server/SnowPeakServerApplication.java
@@ -1,7 +1,11 @@
 package hu.snowpeak.server;
 
+import hu.snowpeak.server.config.rabbitmq.RabbitMQProducer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SnowPeakServerApplication {
@@ -10,4 +14,12 @@ public class SnowPeakServerApplication {
 		SpringApplication.run(SnowPeakServerApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner runner(RabbitMQProducer rabbitMQProducer) {
+		return args -> {
+			// Add a small delay to ensure RabbitMQ connection is established
+			Thread.sleep(5000);
+			rabbitMQProducer.sendMessage("{\"status\": \"connected to RabbitMQ!\"}");
+		};
+	}
 }
