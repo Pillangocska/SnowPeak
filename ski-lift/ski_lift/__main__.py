dbead752742087f056a93a455790094ffa4d23ad
krisztianszenasi <szenasi999@gmail.com>
2024-11-13 16:40:56 +0100

    ⚡️ perf: fail fast if `lift_id` not provided

--- ski-lift/ski_lift/__main__.py
+++ ski-lift/ski_lift/__main__.py
@@ -7,12 +7,14 @@ from ski_lift.core.view.cli_view import CommandLineInterfaceView
 from ski_lift.use_cases import create_controller, create_pika_producer, setup_sensor, attach_loggers_to
 
 
-def main() -> int:        
+def main() -> int:
+    lift_id: str = get_lift_id_or_exit()
+
     with create_pika_producer() as producer:
-        with create_controller(lift_id=get_lift_id_or_exit(), producer=producer) as controller:
+        with create_controller(lift_id=lift_id, producer=producer) as controller:
             attach_loggers_to(controller, producer)            
             # todo: refactor to be inside controller
-            setup_sensor(lift_id=get_lift_id_or_exit(), pika_producer=producer)
+            setup_sensor(lift_id=lift_id, pika_producer=producer)
             cli = CommandLineInterfaceView(controller=controller)
             cli.start_handling_user_inputs()
             return 0
