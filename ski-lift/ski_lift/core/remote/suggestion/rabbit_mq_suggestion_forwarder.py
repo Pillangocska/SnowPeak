6f87695f0a0b9ba00b483e2c3d4a2e44f69cf20b
krisztianszenasi <szenasi999@gmail.com>
2024-11-14 00:51:34 +0100

    ✨ feat: implement remote suggestion forwarding

--- ski-lift/ski_lift/core/remote/suggestion/rabbit_mq_suggestion_forwarder.py
+++ ski-lift/ski_lift/core/remote/suggestion/rabbit_mq_suggestion_forwarder.py
@@ -6,24 +6,34 @@ from ski_lift.core.remote.suggestion.suggestion import Suggestion
 from ski_lift.core.remote.suggestion.suggestion_forwarder import \
     SuggestionForwarder
 from ski_lift.core.view.base_view import BaseView
+from ski_lift.core.remote.rabbitmq.pika_consumer import PikaConsumer
+import os
 
 
 class RabbitMQSuggestionForwarder(SuggestionForwarder):
 
-    def __init__(self, view: BaseView, lift_id: str) -> None:
-        self._connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
-        self._channel = self._connection.channel()
-        self._channel.exchange_declare(
+    def __init__(self, view: BaseView) -> None:
+        self._consumer: PikaConsumer = PikaConsumer(
             exchange='direct_suggestions',
             exchange_type='direct',
+            route_key=view.lift_id,
+            connection_parameters=pika.ConnectionParameters(
+                host=os.environ.get('RABBITMQ_HOST', 'localhost'),
+                port=int(os.environ.get('RABBITMQ_PORT', 5672)),
+                credentials=pika.PlainCredentials(
+                    username=os.environ.get('RABBITMQ_USER', 'guest'),
+                    password=os.environ.get('RABBITMQ_PASSWORD', 'guest'),
+                )
+            )
         )
-        self._queue = self._channel.queue_declare(lift_id)
-        self._channel.queue_bind(exchange='direct_suggestions', queue=lift_id, routing_key=lift_id)
-        self._channel.basic_consume(queue=lift_id, on_message_callback=self.suggestion_callback, auto_ack=True)
+        self._consumer.register_message_callback(self.suggestion_callback)
         super().__init__(view=view)
 
-    def handle_suggestions(self) -> None:
-        self._channel.start_consuming()
+    def start_handling_suggestions(self) -> None:
+        self._consumer.start()
+
+    def stop_handling_suggestions(self):
+        self._consumer.stop()
 
     def suggestion_callback(self, ch, method, properties, body):
         self._view.display_suggestion(Suggestion.from_json(body.decode('utf-8')))
